f9bcf2e5b2669cd2e25e6285fdd51c4a7940268b
KabongeHajarah <kabongehajarah@gmail.com>
2023-08-15 21:29:41 +0400

    HandleClick

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,3 +1,5 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
     const items =[
         'NewYork',
@@ -5,10 +7,17 @@ function ListGroup() {
         'Nairobi',
         'Paris'
     ];
+let selectedIndex=0;
+
+
   const getMessage=()=>{
     return     items.length===0? <h2>Sorry! No item found</h2>:null
   }
-   
+   //type annotation
+   //Event Handler
+const handleClick =(event:MouseEvent)=> console.log(event);
+
+
   return (
     <>
     <h1>This is List Group component </h1>
@@ -17,13 +26,10 @@ function ListGroup() {
     <ul className="list-group">
  
   { items.map((item,index)=>
-  <li key={item} 
-     onClick={(event)=>{
-          console.log(item ,index);
-          console.log(event);
-          
-    
-  }}>{item}</li>)}
+  <li
+  className= {selectedIndex===index?"list-group-item active" :'list-group-item'}
+  key={item} 
+     onClick={handleClick}>{item}</li>)}
 
     </ul>
     </>
