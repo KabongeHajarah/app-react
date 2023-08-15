24e312088abddf8d90994ba65cb4e0072ea03571
KabongeHajarah <kabongehajarah@gmail.com>
2023-08-15 21:07:25 +0400

    ListGroupComponent

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -5,13 +5,18 @@ function ListGroup() {
         'Nairobi',
         'Paris'
     ];
+  const getMessage=()=>{
+    return     items.length===0? <h2>Sorry! No item found</h2>:null
+  }
    
   return (
     <>
     <h1>This is List Group component </h1>
+
+    {getMessage()}
     <ul className="list-group">
  
-  { items.map(item=><li>{item}</li>)}
+  { items.map(item=><li key={item}>{item}</li>)}
 
     </ul>
     </>
