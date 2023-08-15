9e742f4584cc1ca89bba6087cafa6f8ddc28d481
KabongeHajarah <kabongehajarah@gmail.com>
2023-08-15 20:52:49 +0400

    ListComponent

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,20 @@
 function ListGroup() {
+    const items =[
+        'NewYork',
+        'L.A',
+        'Nairobi',
+        'Paris'
+    ];
+   
   return (
+    <>
+    <h1>This is List Group component </h1>
     <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
+ 
+  { items.map(item=><li>{item}</li>)}
+
     </ul>
+    </>
   );
 }
 export default ListGroup;
